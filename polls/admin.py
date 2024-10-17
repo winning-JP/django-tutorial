43175856186c93ec8d0b239cd62dc097f90ae98c
winningJP <github@winning.moe>
2024-10-17 12:16:05 +0900

    お腹が痛い(3日前から...今日病院行く)

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 
 from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
